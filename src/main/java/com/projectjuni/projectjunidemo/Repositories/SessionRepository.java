d7a39e793b62999503684fbd48442de65e3f73d7
Juni <junimurray@gmail.com>
2020-03-22 17:08:09 +0800

    first commit

---
+++ src/main/java/com/projectjuni/projectjunidemo/Repositories/SessionRepository.java
@@ -0,0 +1,7 @@
+package com.projectjuni.projectjunidemo.Repositories;
+
+import com.projectjuni.projectjunidemo.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class SessionRepository extends JpaRepository<Session, Long> {
+}
---
+++ src/test/java/com/projectjuni/projectjunidemo/ProjectjuniDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjuni.projectjunidemo;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjuniDemoApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
